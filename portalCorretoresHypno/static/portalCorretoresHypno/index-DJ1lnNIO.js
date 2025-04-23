b82a014c3d6dd3920bc08160f4aa2ea4c3f29b70
Kevin Maykel <kevinmaykelgyn@gmail.com>
2025-04-23 18:44:06 +0000

    sla

--- portalCorretoresHypno/static/portalCorretoresHypno/index-DJ1lnNIO.js
+++ portalCorretoresHypno/static/portalCorretoresHypno/index-DJ1lnNIO.js
@@ -45466,7 +45466,7 @@ function DC() {
                                     children: xA.jsx(Ym, {emp: A, obraSelecionada: t}),
                                 }),
                                 xA.jsx("div", {
-                                    id: "div_rodape",
+                                    id: "div_rodapé",
                                     className:
                                         "bg-city-orange-500 text-city-gray-100 text-left p-0 py-3 text-xs lg:p-3",
                                     children: [
