71836876658c1467daaa462eaba877786977be50
Kevin Maykel <kevinmaykelgyn@gmail.com>
2025-06-06 17:28:35 +0000

    sla

--- atualizacao.sh
+++ atualizacao.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-echo "Ativando VPN..."
-sudo openvpn --config /etc/openvpn/client/Hostinger_-_ubuntu-config.ovpn --daemon
-
-# Aguarda VPN estabelecer, verificando porta 1433 no servidor SQL
-echo "Aguardando VPN conectar ao banco de dados..."
-
-MAX_TRIES=12  # aguardar no máximo 60s (12 tentativas x 5s)
-TRY=0
-
-until nc -z -w2 192.168.100.250 1433 || [ $TRY -ge $MAX_TRIES ]; do
-    echo "Tentativa $((TRY+1)) de conexão com 192.168.100.250:1433..."
-    TRY=$((TRY+1))
-    sleep 5
-done
-
-if [ $TRY -ge $MAX_TRIES ]; then
-    echo "❌ Falha ao conectar ao banco de dados via VPN. Abortando script."
-    exit 1
-fi
-
-echo "✅ Conexão VPN e banco detectada. Continuando..."
 
 echo "Ativando ambiente virtual..."
 source /var/www/City-Web/env/bin/activate
