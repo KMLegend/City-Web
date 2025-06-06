4642f7e86169a45e11a7679a792d038a67b025b3
Kevin Maykel <113987414+KMLegend@users.noreply.github.com>
2025-06-06 14:26:57 -0300

    bd

--- Testes.py
+++ Testes.py
@@ -6,7 +6,7 @@ from shareplum import Site, Office365
 from shareplum.site import Version
 import sqlite3
 
-def converter_para_serializavel(self, obj):
+def converter_para_serializavel(obj):
         if isinstance(obj, pd.Timestamp):
             return obj.strftime('%Y-%m-%d')
         elif isinstance(obj, (datetime.date, datetime.datetime)):
@@ -21,8 +21,8 @@ pasta = site.Folder('Documentos Compartilhados/Compartilhado/Comercial')
 df_tabela_vendas = pd.read_excel(pasta.get_file('Planilha padrão -  Tabela de Venda.xlsx'),
                                     sheet_name="Tabela de vendas")
 df_cadastro_vendas_0 = pd.read_excel(pasta.get_file('Planilha padrão -  Tabela de Venda.xlsx'),
-                                    sheet_name="Tabela de vendas")
-print(df_tabela_vendas)
+                                    sheet_name="Cadastro")
+# print(df_tabela_vendas)
 
 con = sqlite3.connect("C:\\Users\\kevin.maykel\\PycharmProjects\\City-Web\\db.sqlite3")
 
@@ -57,17 +57,17 @@ df_consulta["Unidade"] = df_consulta["Unidade"].astype(str)
 df_tabela_vendas_0["Empreendimento"] = df_tabela_vendas_0["Empreendimento"].astype(str)
 df_tabela_vendas_0["Obra"] = df_tabela_vendas_0["Obra"].astype(str)
 df_tabela_vendas_0["Unidade"] = df_tabela_vendas_0["Unidade"].astype(str)
-print(df_tabela_vendas_0)
+# print(df_tabela_vendas_0)
 
 df_tabela_vendas = pd.merge(left=df_tabela_vendas_0, right=df_consulta, how="inner", left_on=["Empreendimento", "Obra", "Unidade"], right_on=["Empresa", "Obra", "Unidade"])
-print(df_tabela_vendas)
+# print(df_tabela_vendas)
 
 if 'Excecao' in df_tabela_vendas.columns:
     df_tabela_vendas = df_tabela_vendas[df_tabela_vendas["Excecao"] != "sim"]
 
 df_cadastro_vendas["Empreendimento"] = df_cadastro_vendas["Empreendimento"].astype(str)
 df_tabela_vendas["Empreendimento"] = df_tabela_vendas["Empreendimento"].astype(str)
-
+print(df_cadastro_vendas)
 df_tabela_vendas = df_tabela_vendas.fillna("NULL")
 df_cadastro_vendas = df_cadastro_vendas.fillna("NULL")
 
@@ -85,5 +85,6 @@ for empreendimento, grupo in df_tabela_vendas.groupby("Empreendimento"):
 
 with open("resultado.json", "w") as f:
     json.dump(json_estrutura, f, indent=4, ensure_ascii=False, default=converter_para_serializavel)
-
+    
+    
 print(json_estrutura)
